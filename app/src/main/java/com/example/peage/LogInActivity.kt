d348e1f9554ed83b231bf31b4ce6c4c67d9b54c6
antoinevdp <antoinevdp@live.fr>
2020-11-22 14:33:48 +0100

    new activity

---
+++ app/src/main/java/com/example/peage/LogInActivity.kt
@@ -0,0 +1,15 @@
+package com.example.peage
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LogInActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.log_in)
+
+    }
+
+}
--- app/src/main/java/com/example/peage/MainActivity.kt
+++ app/src/main/java/com/example/peage/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.peage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goToLogin = findViewById<Button>(R.id.goToLogin)
+        goToLogin.setOnClickListener {
+            startActivity(Intent(this, LogInActivity::class.java))
+
+        }
     }
 }
