3e4166c27e40f2358f41720465d5993e29e89ebb
antoinevdp <antoinevdp@live.fr>
2020-12-08 12:37:13 +0100

    fix color 4 + add permis xml and activity

---
+++ app/src/main/java/com/example/peage/PermisActivity.kt
@@ -0,0 +1,12 @@
+package com.example.peage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PermisActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.permis_camera)
+
+    }
+}
--- app/src/main/java/com/example/peage/SignUpActivity.kt
+++ app/src/main/java/com/example/peage/SignUpActivity.kt
@@ -1,6 +1,8 @@
 package com.example.peage
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity: AppCompatActivity() {
@@ -8,5 +10,11 @@ class SignUpActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
+
+        val goToPermis = findViewById<Button>(R.id.goToPermis)
+        goToPermis.setOnClickListener{
+            startActivity(Intent(this, PermisActivity::class.java))
+        }
     }
-}
+    }
+
