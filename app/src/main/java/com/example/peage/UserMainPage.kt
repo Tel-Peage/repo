cd6fbfb6d7f4be3a8b64661982c5eaaf68dc0333
lukstrikers <lucas.mouquet.lm@gmail.com>
2020-12-14 15:44:57 +0100

    video

--- app/src/main/java/com/example/peage/UserMainPage.kt
+++ app/src/main/java/com/example/peage/UserMainPage.kt
@@ -1,6 +1,7 @@
 package com.example.peage
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,15 @@ class UserMainPage: AppCompatActivity() {
         setContentView(R.layout.user_main_page)
         ///val welcomeText = findViewById<TextView>(R.id.welcome_text)
         ///welcomeText.text = "Bienvenue Nicolas"
+
+        val videoView = findViewById<VideoView>(R.id.vid_main_page)
+        val videoPath = "android.resource://" + getPackageName() + "/" + R.raw.anim_peage
+        var uri = Uri.parse(videoPath)
+        videoView.setVideoURI(uri)
+
+        var mediaController = MediaController(this)
+        videoView.setMediaController(mediaController)
+        mediaController.setAnchorView(videoView)
     }
 
 }
