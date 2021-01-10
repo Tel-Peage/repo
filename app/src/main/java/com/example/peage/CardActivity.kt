c8c8e69a5f42e4e6896f374b8572f55cd61adb03
antoinevdp <antoinevdp@live.fr>
2021-01-10 13:22:00 +0100

    card

--- app/src/main/java/com/example/peage/CardActivity.kt
+++ app/src/main/java/com/example/peage/CardActivity.kt
@@ -1,11 +1,18 @@
 package com.example.peage
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CardActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.card)
+
+        val goToLoginFromCard = findViewById<Button>(R.id.goToLoginFromCard)
+        goToLoginFromCard.setOnClickListener{
+            startActivity(Intent(this,LogInActivity::class.java))
+        }
     }
 }
