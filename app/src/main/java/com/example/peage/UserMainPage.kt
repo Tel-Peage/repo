94bfd70ae01161ec25500797642271130bf118e1
antoinevdp <antoinevdp@live.fr>
2021-01-12 23:06:35 +0100

    omg

--- app/src/main/java/com/example/peage/UserMainPage.kt
+++ app/src/main/java/com/example/peage/UserMainPage.kt
@@ -9,13 +9,19 @@ import android.os.Bundle
 import android.os.Handler
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class UserMainPage: AppCompatActivity() {
+class UserMainPage(): AppCompatActivity() {
 
     lateinit var currentUser_id : String
     lateinit var currentUser_firstname : String
@@ -79,8 +85,25 @@ class UserMainPage: AppCompatActivity() {
             statusAnimation = !statusAnimation
 
 
-
         }
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
+
+
+        val navController = findNavController(R.id.fragment4)
+        bottom_navigation.setupWithNavController(navController)
+
+
+        ///val videoView = findViewById<VideoView>(R.id.vid_main_page)
+        ////val videoPath = "android.resource://" + getPackageName() + "/" + R.raw.anim_peage
+        ///var uri = Uri.parse(videoPath)
+        ///videoView.setVideoURI(uri)
+
+        ///var mediaController = MediaController(this)
+        ///videoView.setMediaController(mediaController)
+        ///mediaController.setAnchorView(videoView)
+
+        // Initializes Bluetooth adapter.
 
     }
 
